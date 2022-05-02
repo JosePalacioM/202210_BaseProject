f4dd25c35863a0b2d10755611c2e7bba6258263f
Jose Alexander Palacio Muñoz <j.palaciom@uniandes.edu.co>
2022-05-01 20:46:02 -0500

    Se arregla vista y pruebas

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -2,16 +2,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { Cafe } from '../cafe';
 import { CafeListComponent } from './cafe-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CafeService } from '../cafe.service';
+import { faker } from '@faker-js/faker';
+
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
+  let cafe: Array<Cafe> = [];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CafeListComponent ]
+      declarations: [ CafeListComponent ],
+      imports: [HttpClientModule],
+      providers: [ CafeService ]
     })
     .compileComponents();
   }));
@@ -19,10 +27,38 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    cafe = [];
+
+  for (let i=0; i<3; i++){
+    cafe.push(
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      )
+    );
+  }
+  component.cafes = cafe;
+
+  fixture.detectChanges();
+  debug = fixture.debugElement;
+});
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Prueba para 3 cafes', () =>{
+    expect(component.cafes.length).toEqual(3);
+  });
+
+  it("Existe una tabla", () => {
+    expect(debug.query(By.css("tbody")).childNodes.length).toBeGreaterThan(0);
+  });
+
 });
